5f77029042f010965259c8f9f45fe50282a2483b
lzxin <2731273161@qq.com>
2016-10-24 20:10:45 +0800

    稍微整理一下

--- binary_search.py
+++ binary_search.py
@@ -2,8 +2,14 @@
 # -*- coding: utf-8 -*-
 
 
-def test_binary_search(arr, n):
-    """middle_index +/- 1造成两次死循环"""
+import cProfile
+import random
+
+from quick_sort import quick_sort   # 导入自己编写的快速排序模块,感觉挺奇妙的
+
+
+def binary_search(arr, n):
+    """middle_index +/-1 造成两次死循环"""
 
     if isinstance(arr, list) and isinstance(n, (int, float)):
         length = len(arr)
@@ -21,28 +27,24 @@ def test_binary_search(arr, n):
                 left_index = middle_index + 1
             else:
                 right_index = middle_index - 1
-        # test_binary_search(arr[left_index:right_index],n)
+        # binary_search(arr[left_index:right_index],n)
         return 'no this number'
     else:
         raise TypeError('type of input is error')
 
+
 if __name__ == '__main__':
 
-    import cProfile
-    import random
-    # 导入自己编写的快速排序模块,感觉挺奇妙的
-    from quick_sort import test_quick_sort
-
-    # 测试函数: test_binary_search()
+    # 测试函数: binary_search()
     arr = []
     for i in range(1000):
         arr.append(random.randint(1, 300))
     for x in range(3):
         random.shuffle(arr)
-    arr = test_quick_sort(arr)
+    arr = quick_sort(arr)
     n = random.randint(1, 300)
     # arr = [99]
     # n = 999
-    result_index = test_binary_search(arr, n)
+    result_index = binary_search(arr, n)
     print(result_index)
-    cProfile.run('test_binary_search(arr, n)')
+    cProfile.run('binary_search(arr, n)')
