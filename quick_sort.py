9e6349f4213eead5d3052d7dac656626a3fea4ed
lzxin <2731273161@qq.com>
2016-10-24 19:55:47 +0800

    稍微整理一下

--- quick_sort.py
+++ quick_sort.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 
-def test_quick_sort(arr):
+import cProfile
+import random
+
+
+def quick_sort(arr):
     """一定要分成三部分(</>/=): 1.函数递归调用少,节省时间;2.有大量重复数据时不会溢出函数栈"""
 
     if isinstance(arr, list):
-        import random
-
         if len(arr) <= 1:
             return arr
         # choice()参数不能是空列表,所以先判断之后再选择基准值,顺序不能倒
@@ -28,19 +30,16 @@ def test_quick_sort(arr):
         # bigger = [x for x in arr if x > pivot]
         # pivot_list = [x for x in arr if x == pivot]
 
-        smaller = test_quick_sort(smaller)
-        bigger = test_quick_sort(bigger)
-
+        smaller = quick_sort(smaller)
+        bigger = quick_sort(bigger)
         return smaller + pivot_list + bigger
     else:
         raise TypeError('type is not list')
 
+
 if __name__ == '__main__':
 
-    import cProfile
-    import random
-
-    # 测试函数: test_quick_sort()
+    # 测试函数: quick_sort()
     # arr = list(range(1000))
     arr = []
     for i in range(1000):
@@ -48,7 +47,7 @@ if __name__ == '__main__':
         arr.append(n)
     for x in range(3):
         random.shuffle(arr)
-    print(arr)
-    result_quick_sort = test_quick_sort(arr)
-    print(result_quick_sort)
-    cProfile.run('test_quick_sort(arr)')
+    print('初始数据:\n', arr)
+    result = quick_sort(arr)
+    print('结果:\n', result)
+    cProfile.run('quick_sort(arr)')
